5481382863a934ce7079e39216162e0153e8771f
Frederik Jacques <frederik@the-nerd.be>
2012-02-04 19:34:21 +0100

    Initial Commit

---
+++ DevineStudents/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DevineStudents
+//
+//  Created by Frederik Jacques on 04/02/12.
+//  Copyright (c) 2012 dev-dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
